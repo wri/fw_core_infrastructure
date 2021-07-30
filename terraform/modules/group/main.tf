81eadd4f7d96669effca67ad13fffef14914768f
Thomas Maschler <tmaschler@wri.org>
2021-07-30 00:29:36 -0400

    use length

--- terraform/modules/group/main.tf
+++ terraform/modules/group/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_group_membership" "default" {
 
 
 resource "aws_iam_group_policy_attachment" "default" {
-  count      = var.policy_arns
+  count      = length(var.policy_arns)
   group      = aws_iam_group.default.name
   policy_arn = var.policy_arns[count.index]
 }
